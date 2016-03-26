ee9abb2337bf5250ec83f27472c131254b2f4464
Sateesh Gullipalli <sateesh.gullipalli@aexp.com>
2016-03-26 13:36:39 +0000

    Added user tab

--- client/js/controllers/UserController.js
+++ client/js/controllers/UserController.js
@@ -1,3 +1,20 @@
-app.controller('UserController',['$scope', function($scope){
-	
+app.controller('UserController',['$scope','User', function($scope,User){
+	$scope.rowCollection = [];
+	$scope.itemsByPage = 10;
+	$scope.isLoading = true;
+	$scope.showAddUpdateUser = false;	
+	User.find({
+		filter: { include: ['customer','roles'] }
+	}).$promise
+		.then(function(response) { 
+		  $scope.rowCollection = [].concat(response);
+		   $scope.displayedCollection = [].concat($scope.rowCollection);
+		  if($scope.rowCollection.length==0){
+				 $scope.error = "No data found!!!";
+			 }
+			 $scope.isLoading = false;
+	  },function( errorMessage ) {
+		  $scope.error = "Error has occurred while loading users!";
+		  $scope.isLoading = false;
+   });
 }]);
