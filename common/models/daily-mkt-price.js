fe9b78419a0c22b75d64484c9f2c4be147b73b80
Sateesh Gullipalli <sateesh.gullipalli@aexp.com>
2016-04-03 16:06:28 +0100

    bulk upload dmps

--- common/models/daily-mkt-price.js
+++ common/models/daily-mkt-price.js
@@ -1,3 +1,77 @@
 module.exports = function(DailyMktPrice) {
-
+DailyMktPrice.uploadDMP = function(dmps, callback) {
+	var existing = [];
+	var retValues = [];
+	var found = [];
+	var len = dmps.length;
+	//results is array of dmps
+	var results = (function(){
+		for(var i=0;i<dmps.length;i++){
+			DailyMktPrice.findOne({ where: {and: [ {itemId:dmps[i].itemId}, {marketId:dmps[i].marketId} ] } }, function(err, res) {
+				existing.push(res);
+				if(--len == 0) {
+					existing.forEach(function(ex,j){
+						if(ex == null){
+							var tempDmp = {
+								price : dmps[j].price,
+								dmpDate : new Date(),
+								updatedTimestamp : new Date().getTime(),
+								itemId : dmps[j].itemId,
+								marketId : dmps[j].marketId,
+								hubId : dmps[j].hubId
+							};
+							found.push(tempDmp);
+						} else {
+							var tempDmp = {
+								price : dmps[j].price,
+								dmpDate : dmps[j].dmpDate,
+								updatedTimestamp : new Date().getTime(),
+								itemId : ex.itemId,
+								marketId : ex.marketId,
+								hubId : ex.hubId,
+								id : ex.id
+							};
+							found.push(tempDmp);
+						}
+					});
+					found.forEach(function(f,ind){
+						retValues.push(DailyMktPrice.upsert(f));
+					});
+					Q.all(retValues).then(function(data){
+						callback(null,data);
+					});
+				}
+			});
+		};	
+	});
+	var ss = results();
+};
+DailyMktPrice.observe('after save', function(ctx, next){
+	var mdl;
+	if(ctx.instance)
+		mdl = ctx.instance;
+	else
+		mdl = ctx.data;
+	var app = DailyMktPrice.app;
+	var DailyMktPriceHistory = app.models.DailyMktPriceHistory;
+	var dmpHistory = {
+		price : mdl.price,
+		dmpDate : mdl.dmpDate,
+		updatedTimestamp : mdl.updatedTimestamp,
+		itemId : mdl.itemId,
+		marketId : mdl.marketId
+	};
+	DailyMktPriceHistory.create(dmpHistory, function(err, dmp){
+		
+	});
+	next();
+});
+DailyMktPrice.remoteMethod(
+        'uploadDMP', 
+        {
+		  accepts: {arg: 'dmps', type: 'array'},
+          returns: {arg: 'dmps', type: 'array'},
+		  http: {path:'/uploadDMP', verb: 'post'}
+        }
+    );
 };
