8918b90bba4e5d4a63aedd7364bd5c525a0719e8
Sateesh Gullipalli <sateesh.gullipalli@aexp.com>
2016-04-09 02:04:53 +0100

    added user profile

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,16 @@
 module.exports = function(User) {
 
+User.profile = function(userId, cb) {
+	User.findById(userId,{
+	  include:['hubs','roles','customers'],
+	}, cb);
+};
+User.remoteMethod(
+        'profile', 
+        {
+		  accepts: {arg: 'userId', type: 'string'},
+          returns: {arg: 'user', type: 'object'},
+		  http: {path:'/:userId/profile', verb: 'get'}
+        }
+    );
 };
