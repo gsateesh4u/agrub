ac07973dda244e4acdea98b084a19f9f91a11bf4
Sateesh Gullipalli <gsateesh4u@gmail.com>
2016-08-12 21:27:46 +0100

    autoudpate

---
+++ server/boot/autoupdate.js
@@ -0,0 +1,7 @@
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server'));
+var ds = app.datasources.db;
+ds.autoupdate('LineItem', function(err) {
+  if (err) throw err;
+});
